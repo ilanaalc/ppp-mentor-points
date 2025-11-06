44462c86248314eb9234f9371e21496719edf348
Ilana Alcantara <ilanaalcantara4@gmail.com>
2025-11-06 02:32:07 -0300

    Adicionando novas fixtures de desafios e novas validações das operações de desafios

--- tests/desafios.test.js
+++ tests/desafios.test.js
@@ -2,6 +2,9 @@ const request = require('supertest');
 const { expect } = require('chai');
 const { cadastrarAdmin, cadastrarAluno } = require('../helpers/cadastrarUsuario');
 const { gerarTokenAdmin, gerarTokenAluno } = require('../helpers/gerarToken');
+const postDesafiosSemCupomExtra = require('../fixtures/postDesafiosSemCupomExtra.json');
+const postDesafiosComCupomExtra = require('../fixtures/postDesafiosSemCupomExtra.json');
+
 require('dotenv').config();
 
 describe('Desafios', () => {
@@ -14,12 +17,7 @@ describe('Desafios', () => {
                 .post('/challenges')
                 .set('Content-Type', 'application/json')
                 .set('Authorization', `Bearer ${token}`)
-                .send({
-                    title: "Desafio 1 - Testes de API Rest com SuperTest",
-                    description: "Desenvolva Testes de API Rest com SuperTest",
-                    classId: "T2",
-                    extraCoupon: false
-                });
+                .send(postDesafiosSemCupomExtra);
 
             expect(response.statusCode).to.be.eql(201);
             expect(response.body).to.have.property('id');
@@ -52,12 +50,7 @@ describe('Desafios', () => {
                 .post('/challenges')
                 .set('Content-Type', 'application/json')
                 .set('Authorization', `Bearer ${token}`)
-                .send({
-                    title: "Desafio 2 - Testes de API Rest com Cypress",
-                    description: "Desenvolva Testes de API Rest com Cypress",
-                    classId: "T2",
-                    extraCoupon: false
-                });
+                .send(postDesafiosSemCupomExtra);
 
             expect(response.statusCode).to.be.eql(403);
             expect(response.body.error).to.be.equal('Acesso restrito a administradores');
@@ -68,12 +61,7 @@ describe('Desafios', () => {
             const response = await request(process.env.API_BASE_URL)
                 .post('/challenges')
                 .set('Content-Type', 'application/json')
-                .send({
-                    title: "Desafio 2 - Testes de API Rest com Cypress",
-                    description: "Desenvolva Testes de API Rest com Cypress",
-                    classId: "T2",
-                    extraCoupon: false
-                });
+                .send(postDesafiosComCupomExtra);
 
             expect(response.statusCode).to.be.eql(401);
             expect(response.body.error).to.be.equal('Token não fornecido');
@@ -87,10 +75,153 @@ describe('Desafios', () => {
             let token = await gerarTokenAluno();
 
             const response = await request(process.env.API_BASE_URL)
-                .get('/challenges/T2')
-                .set('Content-Type', 'application-json')
-                .set('Authorization', `Bearer ${token}`)
-                expect(response.statusCode).to.be.eql(200);
+            .get('/challenges/T2')
+            .set('Content-Type', 'application/json')
+            .set('Authorization', `Bearer ${token}`)
+
+            expect(response.statusCode).to.be.eql(200);
         });
-    })
+
+        it('018 - Deve retornar 403 e mensagem de erro ao consultar desafios por um aluno informando uma turma que não faz parte', async () => {
+            await cadastrarAluno();
+            let token = await gerarTokenAluno();
+
+            const response = await request(process.env.API_BASE_URL)
+            .get('/challenges/T1')
+            .set('Content-Type', 'application/json')
+            .set('Authorization', `Bearer ${token}`)
+
+            expect(response.statusCode).to.be.eql(403);
+            expect(response.body.error).to.be.equal('Acesso Negado. O aluno não possui vínculo com a turma informada');
+        });
+
+        it('019 - Deve retornar 200 ao consultar desafios por um administrador', async () => {
+            await cadastrarAdmin();
+            let token = await gerarTokenAdmin();
+
+            const response = await request(process.env.API_BASE_URL)
+            .get('/challenges/T2')
+            .set('Content-Type', 'application/json')
+            .set('Authorization', `Bearer ${token}`)
+
+            expect(response.statusCode).to.be.eql(200);
+        });
+
+        it('020 - Deve retornar uma lista vazia ao consultar desafios de uma turma que não possui desafios cadastrados', async () => {
+            await cadastrarAdmin();
+            let token = await gerarTokenAdmin();
+
+            const response = await request(process.env.API_BASE_URL)
+            .get('/challenges/T1')
+            .set('Content-Type', 'application/json')
+            .set('Authorization', `Bearer ${token}`)
+
+            expect(response.body).to.be.lengthOf(0);
+        });
+
+        it('021 - Deve retornar 401 e mensagem de token não informado ao consultar desafios sem informar um token', async () => {
+
+            const response = await request(process.env.API_BASE_URL)
+            .get('/challenges/T1')
+            .set('Content-Type', 'application/json')
+
+            expect(response.statusCode).to.be.eql(401);
+            expect(response.body.error).to.be.equal('Token não fornecido');
+        });
+        
+    });
+
+    describe('PUT /challenges{id}', () => {
+        it('022 - Deve retornar 200 ao editar um desafio existente por um administrador', async () => {
+            await cadastrarAdmin();
+            let token = await gerarTokenAdmin();
+
+            const response = await request(process.env.API_BASE_URL)
+            .put('/challenges/1')
+            .set('Content-Type', 'application/json')
+            .set('Authorization', `Bearer ${token}`)
+            .send(pos)
+            expect(response.statusCode).to.be.eql(200);
+        });
+
+        it('023 - Deve retornar 403 ao editar um desafio existente por um aluno', async () => {
+            await cadastrarAluno();
+            let token = await gerarTokenAluno();
+
+            const response = await request(process.env.API_BASE_URL)
+            .put('/challenges/1')
+            .set('Content-Type', 'application/json')
+            .set('Authorization', `Bearer ${token}`)
+            .send(postDesafiosSemCupomExtra)
+
+            expect(response.statusCode).to.be.eql(403);
+            expect(response.body.error).to.be.equal('Acesso restrito a administradores');
+        });
+
+        it('024 - Deve retornar 200 ao editar um desafio existente com os mesmos dados por um administrador', async () => {
+            await cadastrarAdmin();
+            let token = await gerarTokenAdmin();
+
+            const response = await request(process.env.API_BASE_URL)
+            .put('/challenges/1')
+            .set('Content-Type', 'application/json')
+            .set('Authorization', `Bearer ${token}`)
+            .send(postDesafiosSemCupomExtra)
+
+            expect(response.statusCode).to.be.eql(200);
+        });
+
+        it('025 - Deve retornar 401 e mensagem de token não informado ao editar um desafio sem informar o token', async () => {
+
+            const response = await request(process.env.API_BASE_URL)
+            .put('/challenges/1')
+            .set('Content-Type', 'application/json')
+            .send(postDesafiosSemCupomExtra)
+
+            expect(response.statusCode).to.be.eql(401);
+            expect(response.body.error).to.be.equal('Token não fornecido');
+        });
+    });
+
+
+    describe('DELETE /challenges{id}', () => {
+        it('026 - Deve retornar 204 ao remover um desafio existente por um administrador', async () => {
+            await cadastrarAdmin();
+            let token = await gerarTokenAdmin();
+
+            const response = await request(process.env.API_BASE_URL)
+            .delete('/challenges/1')
+            .set('Content-Type', 'application/json')
+            .set('Authorization', `Bearer ${token}`)
+
+            expect(response.statusCode).to.be.eql(204);
+         });
+
+        it('027 - Deve retornar 403 e mensagem de acesso não permitido ao remover um desafio existente por um aluno', async () => {
+            await cadastrarAluno();
+            let token = await gerarTokenAluno();
+
+            const response = await request(process.env.API_BASE_URL)
+            .delete('/challenges/1')
+            .set('Content-Type', 'application/json')
+            .set('Authorization', `Bearer ${token}`)
+
+            expect(response.statusCode).to.be.eql(403);
+            expect(response.body.error).to.be.equal('Acesso restrito a administradores');
+         });
+
+        it('028 - Deve retornar 400 e mensagem de desafio não existente ao remover um desafio que não existe', async () => {
+            await cadastrarAdmin();
+            let token = await gerarTokenAdmin();
+
+            const response = await request(process.env.API_BASE_URL)
+            .delete('/challenges/21')
+            .set('Content-Type', 'application/json')
+            .set('Authorization', `Bearer ${token}`)
+
+            expect(response.statusCode).to.be.eql(400);
+            expect(response.body.error).to.be.equal('O desafio informado não existe');
+        });
+    });
 });
+
