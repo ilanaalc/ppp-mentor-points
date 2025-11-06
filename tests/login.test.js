505841717c63cdc798bf54607e298a8016160931
Ilana Alcantara <ilanaalcantara4@gmail.com>
2025-11-05 22:53:04 -0300

    Adicionando novo teste de credenciais inválidas para o POST /users/login

--- tests/login.test.js
+++ tests/login.test.js
@@ -32,5 +32,31 @@ describe('Login', () => {
             expect (response.statusCode).to.be.eql(200);
             expect (response.body).to.have.property('token');
         });
+
+        it('Deve retornar 400 por ausência de campos obrigatórios ao realizar o login', async () => {
+            const response = await request(process.env.API_BASE_URL)
+            .post('/users/login')
+            .set('Content-Type', 'application/json')
+            .send({
+                email: '',
+                password: ''
+            });
+
+            expect (response.statusCode).to.be.eql(400);
+            expect (response.body.error).to.be.equal('E-mail e senha são obrigatórios');
+        });
+
+        it('Deve retornar 401 e mensagem de erro ao informar credenciais inválidas', async () => {
+            const response = await request(process.env.API_BASE_URL)
+            .post('/users/login')
+            .set('Content-Type', 'application/json')
+            .send({
+                email: 'julio@mentoria.com',
+                password: '123'
+            });
+
+            expect (response.statusCode).to.be.eql(401);
+            expect (response.body.error).to.be.equal('Senha inválida');
+        });        
     });
 });
